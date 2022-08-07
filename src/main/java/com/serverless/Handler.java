c7a3bb471a01ba111283ab952cc2154e5baf8783
alba-molina-nyc <avmanyc@gmail.com>
2022-08-07 16:32:00 -0400

    java

---
+++ src/main/java/com/serverless/Handler.java
@@ -0,0 +1,34 @@
+package com.serverless;
+
+import java.util.Collections;
+import java.util.Map;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+public class Handler implements RequestHandler<Map<String, Object>, ApiGatewayResponse> {
+
+	private static final Logger LOG = LogManager.getLogger(Handler.class);
+
+	@Override
+
+	/* ApiGatewayResponse which handles the request */
+	public ApiGatewayResponse handleRequest(Map<String, Object> input, Context context) {
+		LOG.info("received: {}", input);
+		/* create the response body */
+		Response responseBody = new Response("Go Serverless v1.x! Your function executed successfully!", input);
+		return ApiGatewayResponse.builder()
+				.setStatusCode(200)
+				.setObjectBody(responseBody)
+				.setHeaders(Collections.singletonMap("X-Powered-By", "AWS Lambda & serverless"))
+				.build();
+	}
+}
+
+
+/**
+	* same concept but coded diff bc diff language
+*/
